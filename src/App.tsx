e62a1a93b0e78053013bf018d7927e4fed54cd02
DarkDragon009 <nicholasy94@outlook.com>
2026-01-28 06:43:56 -0500

    chore: initialize React app using Typescript and Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Acme Widget Co</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
